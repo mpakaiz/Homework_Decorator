eca5a788ab12863642b6d57c3a91f04eb3a5c562
Pavel Zavartsev <pavel_zavartsev@mail.ru>
2023-06-24 23:25:47 +0300

    removed unneccesary modules

--- task_2.py
+++ task_2.py
@@ -1,10 +1,10 @@
-import os
-import logging
-import datetime
-from functools import wraps
-
-
-
+# import os
+# import logging
+# import datetime
+# from functools import wraps
+#
+#
+#
 # def logger(path):
 #
 #     def __logger(old_function):
@@ -27,8 +27,48 @@ from functools import wraps
 #         return new_function
 #
 #     return __logger
-
-
-
-if __name__ == '__main__':
-    test_2()
+#
+#
+# def test_2():
+#     paths = ('log_1.log', 'log_2.log', 'log_3.log')
+#
+#     for path in paths:
+#         if os.path.exists(path):
+#             os.remove(path)
+#
+#         @logger_foo(path)
+#         def hello_world():
+#             return 'Hello World'
+#
+#         @logger_foo(path)
+#         def summator(a, b=0):
+#             return a + b
+#
+#         @logger_foo(path)
+#         def div(a, b):
+#             return a / b
+#
+#         assert 'Hello World' == hello_world(), "Функция возвращает 'Hello World'"
+#         result = summator(2, 2)
+#         assert isinstance(result, int), 'Должно вернуться целое число'
+#         assert result == 4, '2 + 2 = 4'
+#         result = div(6, 2)
+#         assert result == 3, '6 / 2 = 3'
+#         summator(4.3, b=2.2)
+#
+#     for path in paths:
+#
+#         assert os.path.exists(path), f'файл {path} должен существовать'
+#
+#         with open(path) as log_file:
+#             log_file_content = log_file.read()
+#
+#         assert 'summator' in log_file_content, 'должно записаться имя функции'
+#
+#         for item in (4.3, 2.2, 6.5):
+#             assert str(item) in log_file_content, f'{item} должен быть записан в файл'
+#
+#
+#
+# if __name__ == '__main__':
+#     test_2()
--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -90,4 +90,3 @@ if __name__ == "__main__":
     test_1()
     test_2()
     test_3()
-    
